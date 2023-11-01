a445676a86d99c704fa5d71062eb77f759bf8b69
Christin M <mugishachristin@gmail.com>
2023-11-01 06:20:29 -0400

    Added List

---
+++ src/Lists/ListExample.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Lists;
+
+import java.util.*;
+/**
+ *
+ * @author chris
+ */
+public class ListExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Create a List using ArrayList
+        List<String> myList = new ArrayList<>();
+
+        // Add elements to the list
+        myList.add("apple");
+        myList.add("banana");
+        myList.add("orange");
+
+        // Print the elements of the list
+        System.out.println("List: " + myList);
+
+        // Add an element at a specific index
+        myList.add(1, "grape");
+
+        // Print the list after adding at index
+        System.out.println("List after adding at index: " + myList);
+
+        // Get an element at a specific index
+        String elementAtIndex = myList.get(2);
+        System.out.println("Element at index 2: " + elementAtIndex);
+
+        // Remove an element
+        myList.remove("banana");
+
+        // Print the list after removal
+        System.out.println("List after removal: " + myList);
+
+        // Create a sublist
+        List<String> subList = myList.subList(0, 2);
+        System.out.println("Sublist: " + subList);
+
+        // Set an element at a specific index
+        myList.set(0, "kiwi");
+
+        // Print the list after setting an element
+        System.out.println("List after setting element: " + myList);
+        
+    }
+    
+}
